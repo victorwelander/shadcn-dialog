189fcf79035013b8e3d01077707b4d2a6a07b4a4
Victor Welander <victortwelander@gmail.com>
2024-10-28 21:03:55 +0100

    fixed footer padding

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -6,19 +6,21 @@ import Link from "next/link";
 export default function SiteFooter() {
   return (
     <div className="mt-[164px] border-t p-4">
-      <p className="flex items-center gap-3 text-sm text-muted-foreground">
-        <Image
-          src={avatar}
-          alt="victor welander"
-          width={24}
-          height={24}
-          className="rounded-full"
-        />
-        <span>
-          Made by{" "}
-          <Link href="https://x.com/victorwelander_">Victor Welander</Link>
-        </span>
-      </p>
+      <div className="mx-auto flex w-full max-w-[652px] items-center">
+        <p className="flex items-center gap-3 text-sm text-muted-foreground">
+          <Image
+            src={avatar}
+            alt="victor welander"
+            width={24}
+            height={24}
+            className="rounded-full"
+          />
+          <span>
+            Made by{" "}
+            <Link href="https://x.com/victorwelander_">Victor Welander</Link>
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
