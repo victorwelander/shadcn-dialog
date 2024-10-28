ea5715b465dc782008a236541ba26a4929e8a5cb
Victor Welander <victortwelander@gmail.com>
2024-10-28 14:50:50 +0100

    usage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
 import Examples from "@/components/examples/examples";
 import Hero from "@/components/hero";
+import {
+  InnerDialog,
+  InnerDialogClose,
+  InnerDialogContent,
+  InnerDialogTrigger,
+  StackingDialog,
+  StackingDialogContent,
+  StackingDialogDescription,
+  StackingDialogHeader,
+  StackingDialogTitle,
+  StackingDialogTrigger,
+} from "@/components/ui/dialog-stacking";
 import Usage from "@/components/usage/usage";
 import React from "react";
 
 export default function Index() {
   return (
-    <div className="mx-auto flex w-full max-w-[652px] flex-col justify-center">
+    <div className="mx-auto flex w-full max-w-[652px] flex-col justify-center p-4">
       <Hero />
       <Examples />
       <Usage />
--- components/usage/usage.tsx
+++ components/usage/usage.tsx
@@ -5,20 +5,21 @@ export default function Usage() {
   return (
     <div className="mt-16 flex flex-col">
       <h1 className="text-lg font-medium">Usage</h1>
-      <CodeBlock initialHeight={270}>{`import { Toaster, toast } from 'sonner'
-
-// ...
-
-function App() {
-  return (
-    <div>
-      <Toaster />
-      <button onClick={() => toast('My first toast')}>
-        Give me a toast
-      </button>
-    </div>
-  )
-}`}</CodeBlock>
+      <CodeBlock initialHeight={270}>{`<StackingDialog>
+  <StackingDialogTrigger asChild></StackingDialogTrigger>
+    <StackingDialogContent>
+      <StackingDialogHeader>
+        <StackingDialogTitle></StackingDialogTitle>
+        <StackingDialogDescription></StackingDialogDescription>
+      </StackingDialogHeader>
+      <InnerDialog>
+        <InnerDialogTrigger asChild></InnerDialogTrigger>
+        <InnerDialogContent>
+          <InnerDialogClose></InnerDialogClose>
+      </InnerDialogContent>
+    </InnerDialog>
+  </StackingDialogContent>
+</StackingDialog>`}</CodeBlock>
     </div>
   );
 }
