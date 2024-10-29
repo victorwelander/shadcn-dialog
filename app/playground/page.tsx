8da2aa596a8dbc23eb3d98e76acebfbd127780a7
Victor Welander <victortwelander@gmail.com>
2024-10-29 14:16:02 +0100

    new og image

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,20 +1,11 @@
+import Image from "next/image";
 import React from "react";
-import DefaultDialogPosition from "@/components/positions/default-dialog-position";
-import BottomDialogPosition from "@/components/positions/bottom-dialog-position";
-import TopDialogPosition from "@/components/positions/top-dialog-position";
-import LeftDialogPosition from "@/components/positions/left-dialog-position";
-import RightDialogPosition from "@/components/positions/right-dialog-position";
-import DraggableDialog from "@/components/others/draggable-dialog";
+import dialogs from "@/public/dialogs.svg";
 
 export default function Page() {
   return (
     <div className="flex h-screen items-center justify-center gap-3">
-      <DraggableDialog />
-      <DefaultDialogPosition />
-      <BottomDialogPosition />
-      <TopDialogPosition />
-      <LeftDialogPosition />
-      <RightDialogPosition />
+      <Image src={dialogs} alt="dialogs" width={900} height={900} />
     </div>
   );
 }
